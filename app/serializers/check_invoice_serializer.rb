25d3883efca832487c8480058cbcfdddf377bf4a
fredbanda <fredcbanda@hotmail.com>
2025-04-06 13:10:28 +0200

    fully developed app for test validation

--- app/serializers/check_invoice_serializer.rb
+++ app/serializers/check_invoice_serializer.rb
@@ -1,3 +1,3 @@
 class CheckInvoiceSerializer < ActiveModel::Serializer
-  attributes :id, :check_id
+  attributes :id, :check_id, :invoice_id
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:3001"
+    origins "https://market-link-0czv.onrender.com", "https://skill-validation-test.vercel.app"
 
     resource "*",
       headers: :any,
