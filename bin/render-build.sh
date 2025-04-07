8ffd9041296d0aaa1e62a2b03fa345298835692a
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2025-04-07 15:00:33 +0200

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# exit on error 
-
+# exit on error
 set -o errexit
 
 bundle install
+# Only uncomment this line for one deployment, then comment it out again
+bundle exec rails g solid_queue:install
 bundle exec rake db:migrate
