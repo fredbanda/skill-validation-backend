f86bdf881c0fe25bb8eb2120c304f41ae538d06e
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2025-04-08 22:22:18 +0200

    build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,5 @@
 set -o errexit
 
 bundle install
-# Only uncomment this line for one deployment, then comment it out again
-#bundle exec rails g solid_queue:install
+bundle exec rails solid_queue:install:migrations
 bundle exec rake db:migrate
